e82d67f98063f4ba6daf05a77e7bebcab1cb0189
Juho S <juho.salli@gmail.com>
2024-11-06 11:17:40 +0200

    lisää pointerin 05 harkkaan

--- harjoitukset/05-tyylit/src/SuperPerson.jsx
+++ harjoitukset/05-tyylit/src/SuperPerson.jsx
@@ -1,7 +1,7 @@
 function SuperPerson({ name, superpower }) {
   return (
     <li
-      className="p-2 bg-gray-200 rounded-md hover:bg-gray-300"
+      className="p-2 bg-gray-200 rounded-md hover:bg-gray-300 hover:cursor-pointer"
       onClick={() => {
         deletePerson({ name, superpower });
       }}
